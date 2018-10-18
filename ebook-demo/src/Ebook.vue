bf4965c82b04000ec7c48581b94c3e285295eb1e
earthaYan <eartha_yan@yeah.net>
2018-10-18 22:05:09 +0800

    电子书的解析和渲染功能实现

---
+++ ebook-demo/src/Ebook.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/history.epub'
+global.Epub = Epub
+export default{
+  methods: {
+    showEpub () {
+      this.book = new Epub(DOWNLOAD_URL)
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      this.rendition.display()
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+</script>
+<style lang='scss' scoped>
+@import 'assets/styles/global'
+</style>
