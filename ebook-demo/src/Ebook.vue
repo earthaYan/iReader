bdf837e57aab31deb8ea6fbb2bc968020fc98f96
earthaYan <eartha_yan@yeah.net>
2018-10-21 20:58:32 +0800

    标题栏和菜单栏以及transition的使用

--- ebook-demo/src/Ebook.vue
+++ ebook-demo/src/Ebook.vue
@@ -1,8 +1,48 @@
 <template>
   <div class="ebook">
+    <transition name="slide-down">
+      <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="left">
+          <span class="icon-back icon"></span>
+        </div>
+        <div class="right">
+          <div class="icon-wrapper">
+            <span class="icon-cart icon"></span>
+          </div>
+          <div class="icon-wrapper">
+            <span class="icon-person icon"></span>
+          </div>
+          <div class="icon-wrapper">
+            <span class="icon-more icon"></span>
+          </div>
+        </div>
+      </div>
+    </transition>
+
     <div class="read-wrapper">
       <div id="read"></div>
+      <div class="mask">
+          <div class="left" @click="prevPage"></div>
+          <div class="center" @click="toggleTitleAndMenu"></div>
+          <div class="right" @click="nextPage"></div>
+      </div>
     </div>
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-a icon">A</span>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -11,7 +51,25 @@ import Epub from 'epubjs'
 const DOWNLOAD_URL = '/static/history.epub'
 global.Epub = Epub
 export default{
+  data () {
+    return {
+      ifTitleAndMenuShow: false
+    }
+  },
   methods: {
+    toggleTitleAndMenu () {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+    },
+    prevPage () {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage () {
+      if (this.rendition) {
+        this.rendition.next()
+      }
+    },
     showEpub () {
       this.book = new Epub(DOWNLOAD_URL)
       this.rendition = this.book.renderTo('read', {
@@ -27,5 +85,81 @@ export default{
 }
 </script>
 <style lang='scss' scoped>
-@import 'assets/styles/global'
+@import 'assets/styles/global';
+.ebook{
+  position:relative;
+  .title-wrapper{
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height:px2rem(48);
+    background: white;
+    box-shadow:0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
+    .left{
+      flex:0 0 px2rem(60);
+      @include center;
+    }
+    .right{
+      flex:1;
+      display: flex;
+      justify-content: flex-end;
+      .icon-wrapper{
+        flex:0 0 px2rem(40);
+        @include center;
+      }
+    }
+  }
+  .read-wrapper{
+    .mask{
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index:100;
+      width:100%;
+      height: 100%;
+      display: flex;
+      .left{
+        flex:0 0 px2rem(100);
+      }
+      .left:hover{
+        background: gray;
+        opacity: 0.5;
+      }
+      .center{
+        flex:1;
+      }
+      .right{
+        flex:0 0 px2rem(100);
+      }
+      .right:hover{
+        background: gray;
+        opacity: 0.5;
+      }
+    }
+  }
+  .menu-wrapper{
+    position: absolute;
+    bottom:  0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height:px2rem(48);
+    background: white;
+    box-shadow:0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+    .icon-wrapper{
+      flex: 1;
+      @include center;
+      .icon-progress{
+        font-size: px2rem(24);
+      }
+      .icon-bright{
+        font-size: px2rem(24);
+      }
+    }
+  }
+}
 </style>
