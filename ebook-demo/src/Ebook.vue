c2e8fa2efb4e34053583ec9719474f3629ce773a
earthaYan <yueyueyan1215@163.com>
2018-10-22 22:22:55 +0800

    添加了两个组件，开始字体大小设置功能

--- ebook-demo/src/Ebook.vue
+++ ebook-demo/src/Ebook.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="ebook">
-    <transition name="slide-down">
-      <div class="title-wrapper" v-show="ifTitleAndMenuShow">
-        <div class="left">
-          <span class="icon-back icon"></span>
-        </div>
-        <div class="right">
-          <div class="icon-wrapper">
-            <span class="icon-cart icon"></span>
-          </div>
-          <div class="icon-wrapper">
-            <span class="icon-person icon"></span>
-          </div>
-          <div class="icon-wrapper">
-            <span class="icon-more icon"></span>
-          </div>
-        </div>
-      </div>
-    </transition>
-
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
     <div class="read-wrapper">
       <div id="read"></div>
       <div class="mask">
@@ -27,30 +9,21 @@
           <div class="right" @click="nextPage"></div>
       </div>
     </div>
-    <transition name="slide-up">
-      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
-        <div class="icon-wrapper">
-          <span class="icon-menu icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-progress icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-bright icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-a icon">A</span>
-        </div>
-      </div>
-    </transition>
+    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" ref="menuBar"></menu-bar>
   </div>
 </template>
 
 <script>
+import TitleBar from '@/components/TitleBar.vue'
+import MenuBar from '@/components/MenuBar.vue'
 import Epub from 'epubjs'
 const DOWNLOAD_URL = '/static/history.epub'
 global.Epub = Epub
 export default{
+  components: {
+    TitleBar,
+    MenuBar
+  },
   data () {
     return {
       ifTitleAndMenuShow: false
@@ -59,6 +32,9 @@ export default{
   methods: {
     toggleTitleAndMenu () {
       this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (this.ifTitleAndMenuShow) {
+        this.$refs.MenuBar.hideSetting()
+      }
     },
     prevPage () {
       if (this.rendition) {
@@ -88,30 +64,6 @@ export default{
 @import 'assets/styles/global';
 .ebook{
   position:relative;
-  .title-wrapper{
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 101;
-    display: flex;
-    width: 100%;
-    height:px2rem(48);
-    background: white;
-    box-shadow:0 px2rem(8) px2rem(8) rgba(0, 0, 0, 0.15);
-    .left{
-      flex:0 0 px2rem(60);
-      @include center;
-    }
-    .right{
-      flex:1;
-      display: flex;
-      justify-content: flex-end;
-      .icon-wrapper{
-        flex:0 0 px2rem(40);
-        @include center;
-      }
-    }
-  }
   .read-wrapper{
     .mask{
       position: absolute;
@@ -137,27 +89,6 @@ export default{
       .right:hover{
         background: gray;
         opacity: 0.5;
-      }
-    }
-  }
-  .menu-wrapper{
-    position: absolute;
-    bottom:  0;
-    left: 0;
-    z-index: 101;
-    display: flex;
-    width: 100%;
-    height:px2rem(48);
-    background: white;
-    box-shadow:0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
-    .icon-wrapper{
-      flex: 1;
-      @include center;
-      .icon-progress{
-        font-size: px2rem(24);
-      }
-      .icon-bright{
-        font-size: px2rem(24);
       }
     }
   }
